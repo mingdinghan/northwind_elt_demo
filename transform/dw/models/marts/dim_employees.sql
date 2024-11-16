1b177fb29e70cf269db1c7dd08e47a1c8d655a8e
Mingding Han <mingdinghan@gmail.com>
2024-11-16 20:19:06 +0800

    Add dim_employees

---
+++ transform/dw/models/marts/dim_employees.sql
@@ -0,0 +1,22 @@
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['employees.employee_id']) }} AS employee_key,
+    employees.employee_id,
+    CONCAT(employees.first_name, ' ', employees.last_name) as employee_name,
+    employees.title,
+    employees.birth_date,
+    employees.hire_date,
+    employees.city,
+    employees.region,
+    us_states.state_name AS us_state,
+    employees.country,
+    territories.territory_description,
+    region.region_description
+FROM {{ ref('employees') }} AS employees
+INNER JOIN {{ ref('employee_territories') }} AS et
+    ON employees.employee_id = et.employee_id
+INNER JOIN {{ ref('territories') }} AS territories
+    ON et.territory_id = territories.territory_id
+INNER JOIN {{ ref('region') }} AS region
+    ON territories.region_id = region.region_id
+LEFT JOIN {{ ref("us_states") }} AS us_states
+    ON employees.region = us_states.state_abbr
